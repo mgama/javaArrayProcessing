728ca5c560a73443ac532cd489846911ffbf5079
Familia Gama <manuelgama@Manuels-iMac.local>
2017-06-20 23:17:55 -0400

    Adding first Interface implementation

---
+++ src/ArrayProcessingInterface.java
@@ -0,0 +1,6 @@
+public interface ArrayProcessingInterface {
+
+    public void getLowestValueFromIntArray(ArrayList<Integer> array);
+
+    public void getSecondLowestValueFromIntArray(ArrayList<Integer> array);
+}
