48b29602b1536da744490ff10dba21e94ed2de07
Manuel Gama <manuel.gama@jibo.com>
2017-06-23 10:16:03 -0400

    First Attempt on QuickSort method. More work to do

--- src/main/java/javaArrayProcessing/ArrayProcessor.java
+++ src/main/java/javaArrayProcessing/ArrayProcessor.java
@@ -31,7 +31,7 @@ public class ArrayProcessor {
         	return array[0];
         }
     	if(array.length > 1){
-        	System.out.println("The array length is" + array.length);
+        	System.out.println("The array length is " + array.length);
         	System.out.println(Arrays.toString(array));//For Debug Only
 //        	int[] orderedArray = orderArrayFromLowestToBiggest(array);
         	int[] orderedArray = bubbleSort(array);
@@ -66,7 +66,7 @@ public class ArrayProcessor {
         	throw new IllegalArgumentException("Only 1 int value was passed, at least 2 ints are required");
         }
     	if(array.length > 1){
-        	System.out.println("The array length is" + array.length);
+        	System.out.println("The array length is " + array.length);
         	System.out.println(Arrays.toString(array));//For Debug Only
 //        	int[] orderedArray = orderArrayFromLowestToBiggest(array);
         	int[] orderedArray = bubbleSort(array);
@@ -76,73 +76,76 @@ public class ArrayProcessor {
     	throw new IllegalArgumentException("Error: The argument passed is not an array of Integers");
     }
 
-/*    public int[] orderArrayFromLowestToBiggest(int[] array){
-    	System.out.println("From the orderArrayFromBiggestToLowest");
-    	int[] sortedArray = new int[array.length]; 
-		boolean finishedSorting = false;
+    //This is an attempt to create a quicksort method, which is more efficient than bubbleSort
+    //This is also called the divide and conquer method since it is dividing in blocks 
+    //the whole array and using a pivot element (which is the middle of the array) to start 
+    //the comparisons
+    /*public int[] orderArrayFromLowestToBiggest(int[] array){
 		int pivot = array[(array.length/2)];
 		System.out.println("The pivot is " + pivot);
-		int leftEnd = array[0];
-		System.out.println("The leftEnd is " + leftEnd);
-		int rightEnd = array[array.length-1];
-		System.out.println("The rightEnd is " + rightEnd);
+		int []leftEndArray = new int[array.length/2];
+		int []rightEndArray = new int[array.length/2];
 		
-		//For the first half of the numbers
+		System.out.println("Analyzing the leftEnd subarray");
+		//For the first subarray from the left
 		for (int i = 0; i < (array.length/2); i++){
-			System.out.println("Inside for loop sorting");
-			System.out.println(Arrays.toString(sortedArray));
-			leftEnd = array[i];
-			System.out.println("The leftEnd is " + leftEnd);
-			System.out.println("The rightEnd is " + rightEnd);
-			if (leftEnd > pivot){
-				System.out.println("Moving leftEnd to the right of the pivot");
-				sortedArray[sortedArray.length-1] = leftEnd;
+			int element = array[i];
+			System.out.println("Comparing " + element + " with pivot " + pivot);
+			if (element > pivot){
+				System.out.println("Moving element to the right of the pivot");
+				rightEndArray[i] = element;
+				System.out.println("The rightEndArray is " + Arrays.toString(rightEndArray));
 			}
 			else{
-				sortedArray[i]=leftEnd;
+				leftEndArray[i] = element;
+				System.out.println("The leftEndArray is " + Arrays.toString(leftEndArray));
 			}
 		}
 		
-		System.out.println("After the first loop the sortedArray" + Arrays.toString(sortedArray));
-		
-		//For the first half of the numbers
-		for (int i = (array.length); i > (array.length/2); i--){
-			System.out.println("Inside second loop sorting");
-			System.out.println(Arrays.toString(sortedArray));
-			rightEnd = array[i];
-			System.out.println("The leftEnd is " + leftEnd);
-			System.out.println("The rightEnd is " + rightEnd);
-			if (rightEnd < pivot){
-				System.out.println("Moving leftEnd to the right of the pivot");
-				sortedArray[sortedArray.length-1] = rightEnd;
+		//For the second subarray from the right
+		System.out.println("Moving on to analyze the rightEnd subarray");
+		for (int i = rightEndArray.length + 1; i > (array.length/2); i++){
+			int element = array[i];
+			System.out.println("Comparing " + element + " with pivot " + pivot);
+			if (element > pivot){
+				System.out.println("Moving element to the right of the pivot");
+				rightEndArray[i] = element;
+				System.out.println("The rightEndArray is " + Arrays.toString(rightEndArray));
 			}
 			else{
-				sortedArray[i]=rightEnd;
+				System.out.println("Moving element to the leftEnd array");
+				leftEndArray[(leftEndArray.length + 1)] = element;
+				System.out.println("The leftEndArray is " + Arrays.toString(leftEndArray));
 			}
 		}
-		System.out.println("After the second loop the sortedArray" + Arrays.toString(sortedArray));
 		
+		int[] sortedArray = new int[array.length];
 		return sortedArray;
     }*/
     
     /**
-	 * Returns an array of integers that has been sorted via a bubble sort methodology
+	 * Returns an array of integers that has been sorted via a bubble sort methodology.
+	 * This method of sorting involves comparing each element of the array to the next element;
+	 * if the element is bigger than the next one, then it is swapped. This goes in a cycle and 
+	 * the next element in the array is used as a base to be compared.
 	 *
 	 * @param  array  an array of integers
 	 * @return array returns the same array passed but in order from lowest to highest
 	 */
     public int[] bubbleSort(int[] array){
+    	int temp = 0;//Creating temporary int element, which will be used for swapping on the new array
     	for (int i = (array.length - 1); i >= 0; i--){
-	      for (int j = 1; j <= i; j++)
-	      {
-	    	 System.out.println("Comparing " + array[j-1] + " with " + array[j]);
-	         if (array[j-1] > array[j])
-	         {
-	              int temp = array[j-1];
+	      for (int j = 1; j <= i; j++){
+	    	 System.out.println("Comparing element " + (j-1) + " : " + array[j-1] + " with element " + j + " : " + array[j]);
+	         if (array[j-1] > array[j]){
+	        	  System.out.println("Swapping elements. Moving element " + array[j-1] + " to the right of the array.");
+	              temp = array[j-1];
 	              array[j-1] = array[j];
 	              array[j] = temp;
-	         } 
-	      } 
+	              System.out.println("After swapping, the array is" + Arrays.toString(array));
+	         }
+	      }
+	      System.out.println("Cycle number " + i);
     	}
     	System.out.println("The new sorted array thru bubbleSort is " + Arrays.toString(array));
     	return array;
